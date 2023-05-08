230d4e4036a2ce98b056ad4718852fc164b13d17
ErTiwariAnkit <80820982+ErTiwariAnkit@users.noreply.github.com>
2023-05-08 15:02:48 +0530

    Update settings.py

--- ecomweb/ecomweb/settings.py
+++ ecomweb/ecomweb/settings.py
@@ -79,23 +79,23 @@ WSGI_APPLICATION = 'ecomweb.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': BASE_DIR / 'db.sqlite3',
-#    }
-# }
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'eshopd',
-        'USER': 'django',
-        'PASSWORD': 'Django@123',
-        'HOST': 'localhost',
-        'PORT': '3306'
-    }
+   'default': {
+       'ENGINE': 'django.db.backends.sqlite3',
+       'NAME': BASE_DIR / 'db.sqlite3',
+   }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'eshopd',
+#         'USER': 'django',
+#         'PASSWORD': 'Django@123',
+#         'HOST': 'localhost',
+#         'PORT': '3306'
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
